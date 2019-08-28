4cae22526c26ae840c12bc0c75415351e343ceed
Filipe Constantinov Menezes <fconstantinovmenezes@zendesk.com>
2019-08-28 10:54:42 +0100

    Show extra fields in a call

--- src/templates/call.js
+++ src/templates/call.js
@@ -1,7 +1,10 @@
 export default function (args) {
   return `<div class="talk-partner-app">
     <div>
-      <span>Call requested: ${args.dialout.number}</span>
+      <p>Call requested</p>
+      <p>Number: ${args.dialout.number}</p>
+      <p>User ID: ${args.dialout.userId}</p>
+      <p>Ticket ID: ${args.dialout.ticketId}</p>
     </div>
     <div class="pull-down">
       <button class="c-btn" id="back">&lt; Back</button>
